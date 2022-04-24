57e353799941bf7991d2dc9464f5252f173c651d
Vadim B <vadim.bobrovitsky@gmail.com>
2022-04-24 19:54:37 +0300

    add NewsList component to index page - feature switch page title by locale

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,31 @@
 import Layout from "components/Layout";
+import NewsList from "components/NewsList";
 
-const index = () => {
+import { GetServerSideProps } from 'next'
+
+const titleLocales = {
+  ru: 'Главная',
+  en: 'Main'
+}
+
+const index = (props) => {  
   return (
-    <Layout title="Новости и события">
-
+    <Layout title={titleLocales[props.locale]}>
+      <div className="container">
+        <NewsList />
+      </div>
     </Layout>
   );
 };
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const {locale} = context
+
+  return {
+    props: {
+      locale
+    }
+  }
+}
 
 export default index;
