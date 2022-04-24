9cef0eb6870199b05acaf6ebf0d76877ac868aaf
Vadim B <vadim.bobrovitsky@gmail.com>
2022-04-24 12:38:22 +0300

    add LangSwitcher component in Layout - add reset and variables styles - add header styles in styles.scss

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -2,6 +2,7 @@ import React, { ReactNode } from 'react';
 import Link from 'next/link';
 import Head from 'next/head';
 import Image from 'next/image'
+import LangSwitcher from 'components/LangSwitcher'
 
 type Props = {
   children?: ReactNode
@@ -17,12 +18,16 @@ const Layout = ({ children, title = '' }: Props) => {
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       </Head>
       <header>
-        <div className='container'>
+        <div className='container header-inner'>
           <Link href='/' passHref>
             <a>
-              <Image src='/logo.svg' alt='logo' width={160} height={16}/>
+              <Image src='/img/logo.svg' alt='logo' width={160} height={16} priority/>
             </a>
           </Link>
+
+          <div className='header-lang'>
+            <LangSwitcher />
+          </div>
         </div>
       </header>
       {children}
