fcf2e31c4d2ecd8a0ac3686b481203cdd2a09c8a
Vadim B <vadim.bobrovitsky@gmail.com>
2022-04-27 20:59:14 +0300

    format Layout

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,12 +1,12 @@
 import React, { ReactNode } from 'react';
 import Link from 'next/link';
 import Head from 'next/head';
-import Image from 'next/image'
-import LangSwitcher from 'components/LangSwitcher'
+import Image from 'next/image';
+import LangSwitcher from 'components/LangSwitcher';
 
 type Props = {
-  children?: ReactNode
-  title?: string
+  children?: ReactNode;
+  title?: string;
 };
 
 const Layout = ({ children, title = '' }: Props) => {
@@ -18,14 +18,14 @@ const Layout = ({ children, title = '' }: Props) => {
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       </Head>
       <header>
-        <div className='container header-inner'>
-          <Link href='/' passHref>
+        <div className="container header-inner">
+          <Link href="/" passHref>
             <a>
-              <Image src='/img/logo.svg' alt='logo' width={160} height={16} priority/>
+              <Image src="/img/logo.svg" alt="logo" width={160} height={16} priority />
             </a>
           </Link>
 
-          <div className='header-lang'>
+          <div className="header-lang">
             <LangSwitcher />
           </div>
         </div>
